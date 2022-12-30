325796928de8c00a509492c502a598d8196a86cf
FHausen <frederik_peer.hausen@smail.th-koeln.de>
2022-12-30 18:33:24 +0100

    add Login PoC

---
+++ PoC/Login/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
+    implementation ("com.google.code.gson:gson:2.8.5")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
