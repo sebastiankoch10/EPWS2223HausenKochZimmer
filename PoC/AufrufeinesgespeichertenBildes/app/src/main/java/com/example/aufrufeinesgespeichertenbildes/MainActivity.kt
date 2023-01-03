0422b238d944195a5c33ee2950529de02ccc4095
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-03 11:01:25 +0100

    Anlegen des POC •	Aufruf eines gespeicherten Bildes

---
+++ PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.aufrufeinesgespeichertenbildes
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import com.example.aufrufeinesgespeichertenbildes.databinding.ActivityMainBinding
+import java.io.FileInputStream
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            //TODO()
+            //TODO(Lese JSON von Device)
+            val fileInputStream = FileInputStream(path)
+            val bytes = fileInputStream.readBytes()
+            fileInputStream.close()
+
+            val jsonString = String(bytes)
+            //TODO(parsen des Bildes)
+            //TODO(Aus gabe als Bild)
+            //ImageView funtion
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
