55337e280a274c53db4f9f5ac2cac93814249e9d
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-04 16:13:48 +0100

    Aufruf von gespeicherten json
    Json liegt in einem nicht zugriffsberechtigten speicher

--- PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
+++ PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.aufrufeinesgespeichertenbildes
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.ImageView
 import com.example.aufrufeinesgespeichertenbildes.databinding.ActivityMainBinding
-import java.io.FileInputStream
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,16 +33,24 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             //TODO()
             //TODO(Lese JSON von Device)
-            val fileInputStream = FileInputStream(path)
-            val bytes = fileInputStream.readBytes()
-            fileInputStream.close()
+            val filePath =
+                "/storage/emulated/0/Android/data/com.example.pocbildupload/files/Documents/"
+            val fileName = "outputPic2650852323735112380.json"
+            readJson(filePath,fileName)
 
-            val jsonString = String(bytes)
+
+
             //TODO(parsen des Bildes)
             //TODO(Aus gabe als Bild)
             //ImageView funtion
 
         }
+    }
+
+    fun readJson(filePath: String, fileName: String): Any {
+        val objectMapper = ObjectMapper().registerModule(KotlinModule())
+        val file = File(filePath+ fileName)
+        return objectMapper.readValue(file, Any::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
