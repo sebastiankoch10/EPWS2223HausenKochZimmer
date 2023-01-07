a3fd9fc1ad6286a8bcd5858d97e7e746bcece106
sebastiankoch10 <sebastian.koch@smail.th-koeln.de>
2023-01-07 13:47:27 +0100

    Update PubSub

--- PoC/PubSub/app/src/main/java/com/example/pubsub/MainActivity.kt
+++ PoC/PubSub/app/src/main/java/com/example/pubsub/MainActivity.kt
@@ -1,30 +1,59 @@
 package com.example.pubsub
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-
-
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // Liste mit Namen wird erstellt, welche in der Activity gespeichert wird
+    private val namen = mutableListOf<String>()
+    private val name = "Mein Name"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //SubButton
         val button = findViewById<Button>(R.id.subscribe_button)
         button.setOnClickListener {
-            Toast.makeText(this, "Subscribed!", Toast.LENGTH_SHORT).show()
+
+            if (!namen.contains(name)) { //not contains
+                addName(name)
+            } else {
+                removename(name)
+                //     button.text.        //Unsubscribe
+            }
         }
 
+        //PubButton
         val publishButton = findViewById<Button>(R.id.publish_button)
         publishButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            val PICK_IMAGE_REQUEST = 0
-            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+            // Benachrichtigung an alle Namen in Liste
+            for (name in namen) {
+                Toast.makeText(this, "Ein neuer Beitrag in Stadt XY von $name!", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
+    }
+
+    private fun removename(name: String) {
+        namen.remove(name)
+        updateTextView("Subscribe")
+        Toast.makeText(this, "Unsubscribed!", Toast.LENGTH_SHORT).show()
+    }
+
+    // Name wird zur Liste hinzugefügt
+    private fun addName(name: String) {
+        namen.add(name)
+        updateTextView("Unsubscribe")
+        Toast.makeText(this, "Subscribed!", Toast.LENGTH_SHORT).show()
+    }
+
+    //Aktualisieren der TextView, welche die Namen der Subscriber anzeigt
+    private fun updateTextView(input: String) {
+        val textView = findViewById<TextView>(R.id.subscribe_button)
+        textView.text = input
     }
 }
