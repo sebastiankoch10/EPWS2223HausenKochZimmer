a78aaf8d81aecf1b9b6db3fe297170a6e8b99e7c
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-08 07:45:43 +0100

    Zusammenführung abgeschlossen Fehler besteht

--- PoC/Bild_UploadAufruf/app/src/main/java/com/example/bild_uploadaufruf/MainActivity.kt
+++ PoC/Bild_UploadAufruf/app/src/main/java/com/example/bild_uploadaufruf/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bild_uploadaufruf
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +9,10 @@ import android.os.Bundle
 import android.os.Environment
 import android.util.Base64
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import org.json.JSONObject
@@ -36,6 +40,47 @@ class MainActivity : AppCompatActivity() {
             //write jason or override
             //schreiben
             writeToJson(jsonObject)
+
+            val aufrufButton = findViewById<Button>(R.id.aufruf)
+            aufrufButton.setOnClickListener {
+                try {
+                    val filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+                    val fileName = "outputPic6385210105626963512.json"
+                    val file = File(filePath, fileName)
+                    val contentFile = BufferedReader(FileReader(file)).use { it.readText() }
+
+
+                    val jsonObject = JSONObject()
+                    jsonObject.put("jsonImage", contentFile)
+
+
+                    val imageView = ImageView(this)
+                    imageView.layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                    val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
+                    linearLayout.addView(imageView)
+
+
+                    val encodedimage = jsonObject.getString("jsonImage")
+
+                    val imageBytes = Base64.decode(encodedimage, Base64.DEFAULT)
+                    val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                    imageView.setImageBitmap(bitmap)
+
+                } catch (e: FileNotFoundException) {
+                    println("File not found: ${e.message}")
+                } catch (e: IOException) {
+                    println("IO Exception: ${e.message}")
+                }catch (e: IllegalAccessError)  {
+                    println("IllegalAccessError: ${e.message}")
+                } catch (e: IllegalAccessException) {
+                    println("IllegalAccessException: ${e.message}")
+                }
+                catch (e: Exception) {
+                    println("Exception: ${e.message}")
+                }
+            }
         }
     }
 
