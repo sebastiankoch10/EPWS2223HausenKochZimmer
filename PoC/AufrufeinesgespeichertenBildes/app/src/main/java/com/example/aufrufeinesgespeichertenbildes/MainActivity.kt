b25c58a6e2e0f654e0d72d4b447f5065f98f974a
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-22 15:09:30 +0100

    Versuch ausgabe mit asynchron

--- PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
+++ PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.aufrufeinesgespeichertenbildes
 
-import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.aufrufeinesgespeichertenbildes.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
@@ -18,26 +18,19 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import kotlinx.coroutines.*
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
+import org.json.JSONObject
+import java.util.*
 
-@Suppress("NAME_SHADOWING")
 class MainActivity : AppCompatActivity() {
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private val requestReadExternalStorage = 1
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -45,55 +38,19 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener {
-
-            var result = ""
-
-            fun readFromDatabase() {
-                val myRef = FirebaseDatabase.getInstance().getReference("outputPic")
-                myRef.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        val result = dataSnapshot.getValue<String>()
-                        println(result)
-                    }
-
-                    override fun onCancelled(databaseError: DatabaseError) {
-                        // handle error
-                    }
-                })
-
+            coroutineScope.launch {
+                val result = withContext(Dispatchers.IO) {
+                    val myRef = FirebaseDatabase.getInstance().getReference("message")
+                    val snapshot = myRef.awaitValue()
+                    snapshot.getValue<String>().toString()
+                }
+                val imageBytes = Base64.getDecoder().decode(result)
+                val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                val imageView = ImageView(this@MainActivity)
+                imageView.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                )
             }
-
-// use the function
-            readFromDatabase()
-
-// then use the result variable
-            println("Wert ist:"+result)
         }
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
-
-
-
-
