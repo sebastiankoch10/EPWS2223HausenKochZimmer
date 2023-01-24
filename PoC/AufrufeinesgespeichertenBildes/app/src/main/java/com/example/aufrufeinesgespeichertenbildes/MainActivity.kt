f53dea85a9e8c81bb40846518b6760ae1c10e32b
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-24 18:07:06 +0100

    try catch

--- PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
+++ PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.aufrufeinesgespeichertenbildes
 
-import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.aufrufeinesgespeichertenbildes.databinding.ActivityMainBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
 import kotlinx.coroutines.*
-import org.json.JSONObject
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -36,23 +29,32 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        val myRef = FirebaseDatabase.getInstance().getReference("outputPic")
+
         binding.fab.setOnClickListener {
 
-            var result = ""
-
-            fun readFromDatabase() {
-                val myRef = FirebaseDatabase.getInstance().getReference("outputPic")
-                myRef.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        val result = dataSnapshot.getValue<String>()
-                        println(result)
-                    }
-
-                    override fun onCancelled(databaseError: DatabaseError) {
-                        // handle error
-                    }
-                })
+            try {
+                readFromDatabase(myRef)
+            } catch (e: Exception) {
+                // handle error
+                Log.e("MainActivity", "Error reading from database", e)
             }
+
+
         }
     }
+}
+
+fun readFromDatabase(myRef: DatabaseReference) {
+    val listener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            val result = dataSnapshot.getValue<String>()
+            println(result)
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+            // handle error
+        }
+    }
+    myRef.addValueEventListener(listener)
 }
