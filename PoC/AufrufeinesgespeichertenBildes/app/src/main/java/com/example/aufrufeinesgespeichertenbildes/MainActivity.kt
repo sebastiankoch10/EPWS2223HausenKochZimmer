65a78bba478a92c9f30a29d05a26af9be47bb473
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-01-24 20:46:52 +0100

    erster sync mit layout

--- PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
+++ PoC/AufrufeinesgespeichertenBildes/app/src/main/java/com/example/aufrufeinesgespeichertenbildes/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.aufrufeinesgespeichertenbildes
 
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -18,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
 
             try {
-                readFromDatabase(myRef)
+                readFromDatabase(myRef, this)
             } catch (e: Exception) {
                 // handle error
                 Log.e("MainActivity", "Error reading from database", e)
@@ -45,16 +50,33 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-fun readFromDatabase(myRef: DatabaseReference) {
+fun readFromDatabase(myRef: DatabaseReference, activity: MainActivity) {
     val listener = object : ValueEventListener {
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             val result = dataSnapshot.getValue<String>()
-            println(result)
+            showPic(result, activity)
         }
 
         override fun onCancelled(databaseError: DatabaseError) {
             // handle error
+            Log.e("MainActivity", "Database error: ${databaseError.message}")
+            val toast = Toast.makeText(activity, "Error reading from database", Toast.LENGTH_SHORT)
+            toast.show()
         }
     }
     myRef.addValueEventListener(listener)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun showPic(result : String?, activity: MainActivity) {
+    val imageBytes = Base64.getDecoder().decode(result)
+    val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    val imageView = ImageView(activity)
+    imageView.layoutParams = ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+    )
+    imageView.setImageBitmap(bitmap)
+    val linearLayout = findViewById<LinearLayout> (R.id.linear_layout)
+    linearLayout.addView(imageView)
 }
